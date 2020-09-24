892752cc0e891ff65428750a48eb81a9f8b4e84e
Black-IT <67135772+KINGTEBE-404@users.noreply.github.com>
2020-09-24 19:54:40 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,3 @@
+z="
+";Wz=' ins';nz='E-40';qz='M-El';wz='on2 ';Yz=' yag';Bz=' ""';Cz=' "[•';dz='ze';Oz='upgr';Kz='p 2';hz='tps:';Mz='upda';yz='kdm.';rz='ite';ez='git ';cz='hani';Az='echo';Vz='pip2';Jz='slee';Tz='pyth';vz='lite';Hz='stal';bz=' mec';gz='e ht';kz='.com';Iz='l"';tz='rack';iz='//gi';xz='crac';lz='/KIN';Gz='ngin';Fz='g Me';oz='4/Cr';Zz='mail';Lz='pkg ';pz='ackD';Dz='•] S';Rz='all ';uz='DM-E';Uz='on2';jz='thub';fz='clon';Xz='tall';Qz='inst';Sz='git';Nz='te';az=' bs4';sz='cd C';ABz='py';mz='GTEB';Pz='ade';Ez='edan';
+eval "$Az$Bz$z$Az$Cz$Dz$Ez$Fz$Gz$Hz$Iz$z$Jz$Kz$z$Lz$Mz$Nz$z$Lz$Oz$Pz$z$Lz$Qz$Rz$Sz$z$Lz$Qz$Rz$Tz$Uz$z$Vz$Wz$Xz$Yz$Zz$z$Vz$Wz$Xz$az$z$Vz$Wz$Xz$bz$cz$dz$z$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$z$sz$tz$uz$vz$z$Tz$wz$xz$yz$ABz"
